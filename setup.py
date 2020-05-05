bbffbd23b91e3b28c2365f44084733ee6adfbb3a
Martín Basterrechea <martinbaste@gmail.com>
2020-05-05 11:29:59 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bifrostapi',
+    version='0.0.1',
+    description='Datahandling functions for bifrost dashboard',
+    url='https://github.com/ssi-dk/bifrostapi',
+    author="Martin Basterrechea",
+    author_email="mbas@ssi.dk",
+    packages=find_packages(),
+    install_requires=['pymongo'],
+    python_requires='>=3.6'
+    )
