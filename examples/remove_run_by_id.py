2c1b9618b86794261c7237aee87f5e069af6e640
Finn Gruwier Larsen <figl@ssi.dk>
2022-07-26 13:14:16 +0200

    Move ObjectID to API level

--- examples/remove_run_by_id.py
+++ examples/remove_run_by_id.py
@@ -1,10 +1,9 @@
 from os import getenv
-from bson.objectid import ObjectId
 import bifrostapi
 
 # Call this once before making any other calls.
 bifrostapi.add_URI(getenv('MONGO_CONNECTION'))
 
-obj_id = ObjectId(input("Run id: "))
-run = bifrostapi.runs.get_run_by_id(obj_id)
+run_id = input("Run id: ")
+run = bifrostapi.runs.get_run_by_id(run_id)
 print(f"Preparing to remove run with name {run['name']}...")
