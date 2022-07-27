475f4d97d22e6da3f0fc9a22842885dcaa737fa3
Finn Gruwier Larsen <figl@ssi.dk>
2022-07-27 10:17:59 +0200

    --force option.

--- tools/remove_run_by_id.py
+++ tools/remove_run_by_id.py
@@ -11,6 +11,7 @@ bifrostapi.add_URI(getenv('MONGO_CONNECTION'))
 parser = argparse.ArgumentParser(
     description='Script for removing a run and related objects from MongoDB')
 parser.add_argument('run_id', type=str, help='The MongoDB _id field of the run object')
+parser.add_argument('--force', default=False, action='store_true', help='Continue deletion when inconsistences are found')
 args = parser.parse_args()
 
 run = bifrostapi.runs.get_run_by_id(args.run_id)
@@ -28,21 +29,24 @@ for run_sample in run['samples']:
     if sample is None:
         print(f"Consistency warning: a sample that is referenced in the run does not exist in samples collection:")
         print(run_sample)
-        exit(2)
-    if run_sample['name'] != sample['name']:
-        print(f"Consistency warning: name consistency check failed for sample id {run_sample['_id']}.")
-        print(f"Run sample name is {run_sample['name']}")
-        print(f"Sample name is {sample['name']}")
-        exit(3)
+        if not args.force:
+            exit(2)
     else:
+        if run_sample['name'] != sample['name']:
+            print(f"Consistency warning: name consistency check failed for sample id {run_sample['_id']}.")
+            print(f"Run sample name is {run_sample['name']}")
+            print(f"Sample name is {sample['name']}")
+            if not args.force:
+                exit(3)
+    
+         # Sample component documents
         component_names = [component['name'] for component in sample['components']]
         print(sample['name'], component_names)
-    # Sample component documents
-    sample_components = list(bifrostapi.sample_components.find_sample_component_ids_by_sample_id(sample['_id']))
-    sample_component_object_ids = [sc['_id'] for sc in sample_components]
-    for oid in sample_component_object_ids:
-        bifrostapi.sample_components.delete_sample_component_by_id(oid)
-    print(f"Deleted {len(sample_components)} documents from sample_components collection")
+        sample_components = list(bifrostapi.sample_components.find_sample_component_ids_by_sample_id(sample['_id']))
+        sample_component_object_ids = [sc['_id'] for sc in sample_components]
+        for oid in sample_component_object_ids:
+            bifrostapi.sample_components.delete_sample_component_by_id(oid)
+        print(f"Deleted {len(sample_components)} documents from sample_components collection")
 
 print("Deleting sample documents...")
 for run_sample in run['samples']:
